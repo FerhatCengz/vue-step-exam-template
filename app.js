86e344fcfdbb3b97f4d468f57a00c4dea31bd9e5
FerhatCengz <ferhat.cengiz13@outlook.com>
2023-12-12 17:39:46 +0300

    m2

--- app.js
+++ app.js
@@ -28,7 +28,95 @@ const app = Vue.createApp({
           choices: ["Mars", "Jüpiter", "Satürn", "Venüs"],
           correctAnswer: "B",
         },
-        // Diğer soruları ekleyebilirsiniz.
+        {
+          id: 121,
+
+          text: "C vitamini hangi meyvede bulunur?",
+          choices: ["Elma", "Muz", "Üzüm", "Portakal"],
+          correctAnswer: "D",
+        },
+        {
+          id: 221,
+
+          text: "Dünya üzerinde kaç tane kıta bulunmaktadır?",
+          choices: ["3", "5", "7", "9"],
+          correctAnswer: "C",
+        },
+        {
+          id: 321,
+
+          text: "İnsan vücudu kaç kemikten oluşur?",
+          choices: ["150", "206", "300", "400"],
+          correctAnswer: "B",
+        },
+        {
+          id: 421,
+
+          text: "Hangi gezegen Güneş Sistemi'ndeki en büyük gezegen'dir?",
+          choices: ["Mars", "Jüpiter", "Satürn", "Venüs"],
+          correctAnswer: "B",
+        },
+
+        {
+          id: 1,
+          text: "Hangi element periyodik tabloda 'Fe' sembolü ile gösterilir?",
+          choices: ["Fosfor", "Demir", "Flor", "Fermiyum"],
+          correctAnswer: "B",
+        },
+        {
+          id: 2,
+          text: "JavaScript'te bir değişkeni tanımlamak için hangi anahtar kelime kullanılır?",
+          choices: ["let", "var", "const", "int"],
+          correctAnswer: "A",
+        },
+        {
+          id: 3,
+          text: "Karekökü 25 olan sayı nedir?",
+          choices: ["5", "10", "15", "20"],
+          correctAnswer: "A",
+        },
+        {
+          id: 4,
+          text: "Hangi yıl Amerika'nın bağımsızlığını kazandığı yıl olarak bilinir?",
+          choices: ["1776", "1789", "1804", "1856"],
+          correctAnswer: "A",
+        },
+        {
+          id: 5,
+          text: "Dünyanın en yüksek zirvesi neresindedir?",
+          choices: ["Everest Dağı", "K2", "Annapurna", "Kangchenjunga"],
+          correctAnswer: "A",
+        },
+        {
+          id: 6,
+          text: "Beatles hangi ülkeden gelmektedir?",
+          choices: ["İngiltere", "ABD", "Almanya", "Fransa"],
+          correctAnswer: "A",
+        },
+        {
+          id: 7,
+          text: "Leonardo da Vinci'nin ünlü eseri hangisidir?",
+          choices: ["Mona Lisa", "Starry Night", "Guernica", "The Persistence of Memory"],
+          correctAnswer: "A",
+        },
+        {
+          id: 8,
+          text: "Işığın en yavaş hareket ettiği ortam hangisidir?",
+          choices: ["Hava", "Su", "Vakum", "Cam"],
+          correctAnswer: "C",
+        },
+        {
+          id: 9,
+          text: "William Shakespeare'in en ünlü oyunu hangisidir?",
+          choices: ["Romeo ve Juliet", "Hamlet", "Othello", "Macbeth"],
+          correctAnswer: "B",
+        },
+        {
+          id: 10,
+          text: "Futbolda bir takımda kaç oyuncu bulunur?",
+          choices: ["9", "10", "11", "12"],
+          correctAnswer: "C",
+        },
       ],
     };
   },
@@ -49,13 +137,11 @@ const app = Vue.createApp({
       return htmlString.replace(/<\/?[^>]+(>|$)/g, "").trim();
     },
     nextQuestion() {
-      document.querySelectorAll("li[data-num]")[this.currentQuestion].classList.remove("active");
-      document.querySelectorAll("li[data-num]")[this.currentQuestion + 1].classList.add("active");
+      document.querySelector(".paginationjs-next.J-paginationjs-next").click();
       this.currentQuestion++;
     },
     prevQuestion() {
-      document.querySelectorAll("li[data-num]")[this.currentQuestion].classList.remove("active");
-      document.querySelectorAll("li[data-num]")[this.currentQuestion - 1].classList.add("active");
+      document.querySelector(".paginationjs-prev.J-paginationjs-previous").click();
       this.currentQuestion--;
     },
   },
@@ -89,8 +175,8 @@ const app = Vue.createApp({
       dataSource: this.questions,
       pageSize: 1,
       pageNumber: 1,
-      showPrevious: false,
-      showNext: false,
+      // showPrevious: false,
+      // showNext: false,
       callback: function (data, pagination) {
         vm.currentQuestion = pagination.pageNumber - 1;
       },
